f1af4e2e4d749cf2f8ac2e8eaddc5c4840b38245
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2024-08-04 19:55:37 +0530

    Notifiocation page UI fixing -01

--- src/pages/notification/NotificationPage.jsx
+++ src/pages/notification/NotificationPage.jsx
@@ -54,10 +54,10 @@ const NotificationPage = () => {
 	});
 
 	const { data: authUser } = useQuery({ queryKey: ["userAuth"] });
-	
+
 	return (
 		<>
-			<div className="flex-[4_4_0] border-l border-r border-gray-700 min-h-screen">
+			<div className="flex-[4_4_0] border-l border-r border-gray-700 min-h-screen overflow-x-hidden">
 				<div className="flex justify-between items-center p-4 border-b border-gray-700">
 					<p className="font-bold">Notifications</p>
 					<div className="dropdown ">
