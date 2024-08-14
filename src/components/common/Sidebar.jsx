a3d290ad7a3c4f744a2be485536de34b5361bb58
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2024-08-14 15:31:47 +0530

    Added search user feature 04: UI fixed

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -63,8 +63,8 @@ const Sidebar = () => {
 
 	return (
 		<div>
-			<dialog id="my_modal_2" className="modal">
-				<div className="modal-box">
+			<dialog id="my_modal_2" className="modal ">
+				<div className="modal-box absolute z-70 top-12">
 					<form method="dialog">
 						{/* if there is a button in form, it will close the modal */}
 						<button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
